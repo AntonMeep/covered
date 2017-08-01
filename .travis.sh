4b1c1e34aff7d71596c7f00ffb291fc6422adb3e
Anton Fediushin <fediushin.anton@yandex.ru>
2017-08-01 14:21:46 +0200

    Remove build for x86

--- .travis.sh
+++ .travis.sh
@@ -6,8 +6,4 @@ if [ "$DC" == ldc2 ] && [ -n "$TRAVIS_TAG" ]; then
 	echo "Building $TRAVIS_TAG for $TRAVIS_OS_NAME x86_64"
 	dub build -b release --compiler=$DC
 	tar -zcf "covered-$TRAVIS_TAG-$TRAVIS_OS_NAME-x86_64.tar.gz" covered
-
-	echo "Building $TRAVIS_TAG for $TRAVIS_OS_NAME x86"
-	dub build -b release --compiler=$DC --arch=x86
-	tar -zcf "covered-$TRAVIS_TAG-$TRAVIS_OS_NAME-x86.tar.gz" covered
 fi
