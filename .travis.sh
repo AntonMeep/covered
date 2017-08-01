dc3578f24ed5750428a973b5ba17e467aa5a15dd
Anton Fediushin <fediushin.anton@yandex.ru>
2017-08-01 11:55:44 +0200

    Let ldc2 build release build

---
+++ .travis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+echo dub test --compiler=$DC
+
+if [ "$DC" == ldc2 ] && [ -n "$TRAVIS_TAG" ]; then
+	echo "Building $TRAVIS_TAG for $TRAVIS_OS_NAME x86_64"
+	dub build -b release --compiler=$DC
+	tar -zcf "covered-$TRAVIS_TAG-$TRAVIS_OS_NAME-x86_64.tar.gz" covered
+
+	echo "Building $TRAVIS_TAG for $TRAVIS_OS_NAME x86"
+	dub build -b release --compiler=$DC --arch=x86
+	tar -zcf "covered-$TRAVIS_TAG-$TRAVIS_OS_NAME-x86.tar.gz" covered
+fi
