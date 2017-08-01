a92fddffa2fa4cc7fbb30128fdbaf3d85b818957
Anton Fediushin <fediushin.anton@yandex.ru>
2017-08-01 14:39:19 +0200

    Oh again

--- .travis.sh
+++ .travis.sh
@@ -5,13 +5,3 @@ dub test --compiler=$DC
 echo "Building $TRAVIS_TAG for $TRAVIS_OS_NAME x86_64 using $DC"
 dub build -b release --compiler=$DC
 tar -zcf "covered-$TRAVIS_TAG-$TRAVIS_OS_NAME-$DC-x86_64.tar.gz" covered
-
-echo
-echo
-
-if [ "$DC" != ldc ]; then
-	echo "Building $TRAVIS_TAG for $TRAVIS_OS_NAME x86 using $DC"
-	dub build -b release --compiler=$DC --arch=x86
-	tar -zcf "covered-$TRAVIS_TAG-$TRAVIS_OS_NAME-$DC-x86.tar.gz" covered
-fi
-
