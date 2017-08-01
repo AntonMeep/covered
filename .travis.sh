5031351a60464435376ca65e4c3ddf3727c69d7c
Anton Fediushin <fediushin.anton@yandex.ru>
2017-08-01 14:55:58 +0200

    Fix tarball naming

--- .travis.sh
+++ .travis.sh
@@ -2,6 +2,6 @@
 
 dub test --compiler=$DC
 
-echo "Building $TRAVIS_TAG for $TRAVIS_OS_NAME x86_64 using $DC"
+echo "Building $(git tag -l --points-at HEAD) for $TRAVIS_OS_NAME x86_64 using $DC"
 dub build -b release --compiler=$DC
-tar -zcf "covered-$TRAVIS_TAG-$TRAVIS_OS_NAME-$DC-x86_64.tar.gz" covered
+tar -zcf "covered-$(git tag -l --points-at HEAD)-$TRAVIS_OS_NAME-$DC-x86_64.tar.gz" covered
