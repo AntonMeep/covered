c05cfac71f9e1d22d413ae6782738adba35907f1
Anton Fediushin <fediushin.anton@yandex.ru>
2017-08-02 17:52:18 +0200

    Add CoverageLoader2, which will replace CoverageLoader in the future

--- source/app.d
+++ source/app.d
@@ -1,3 +1,24 @@
-import covered.commandline;
+version(unittest) {
+	version(D_Coverage) shared static this() {
+		import core.runtime : dmd_coverDestPath;
+		import std.file : exists, mkdir;
 
-int main(string[] args) { return coveredMain(args); }
+		enum COVPATH = "coverage";
+
+		if(!COVPATH.exists)
+			COVPATH.mkdir;
+		dmd_coverDestPath(COVPATH);
+	}
+
+	import std.stdio : writeln;
+	import unit_threaded : runTests;
+
+	int main(string[] args) {
+		version(D_Coverage) writeln("Running code coverage analysis...");
+		writeln("Running unit tests...");
+		return runTests!("covered.loader")(args);
+	}
+} else {
+	import covered.commandline;
+	int main(string[] args) { return coveredMain(args); }
+}
--- source/covered/loader.d
+++ source/covered/loader.d
@@ -108,3 +108,159 @@ if(isInputRange!T && is(ElementType!T == dchar)) {
 		return 0;
 	}
 }
+
+struct CoverageLoader2 {
+	private {
+		File m_file;
+		char[] m_buffer;
+
+		bool m_coverage_computed = false;
+		float m_coverage;
+
+		bool m_stats_available = 0;
+
+		size_t m_covered;
+		size_t m_total;
+	}
+
+	this(string fname) { this(File(fname, "r")); }
+
+	this(File f) {
+		m_file = f;
+	}
+
+	ByEntryRange byEntry() { return ByEntryRange(m_file); }
+
+	private void getCoveredAndTotalLines() {
+		import std.string : indexOf, stripLeft;
+		import std.conv : to;
+
+		m_buffer.reserve(4096);
+
+		m_covered = m_total = 0;
+		import std.stdio;
+		import std.experimental.logger;
+
+		while(m_file.readln(m_buffer)) {
+			immutable bar = m_buffer.indexOf('|');
+
+			if(bar == -1) {
+				break;
+			} else {
+				auto num = m_buffer[0..bar].stripLeft;
+				if(num.length) {
+					foreach(ref c; num) {
+						if(c != '0') {
+							++m_covered;
+							break;
+						}
+					}
+					++m_total;
+				}
+			}
+		}
+
+		m_stats_available = true;
+	}
+
+	size_t getTotalCount() {
+		if(!m_stats_available)
+			this.getCoveredAndTotalLines();
+
+		return m_total;
+	}
+
+	size_t getCoveredCount() {
+		if(!m_stats_available)
+			this.getCoveredAndTotalLines();
+
+		return m_covered;
+	}
+
+	float getCoverage() {
+		if(!m_stats_available)
+			this.getCoveredAndTotalLines();
+
+		if(!m_coverage_computed)
+			m_coverage = m_covered.to!float / m_total.to!float * 100.0f;
+
+		return m_coverage;
+	}
+}
+
+@(".getCoveredCount, .getTotalCount and .getCoverage produce expected results")
+unittest {
+	auto c = CoverageLoader2("sample/hello.lst");
+	c.getCoveredCount.should.be.equal(1);
+	c.getTotalCount.should.be.equal(1);
+
+	c.getCoverage.should.be.equal(100.0f);
+}
+
+struct Entry {
+	bool Used;
+	size_t Count;
+	string Source;
+}
+
+struct ByEntryRange {
+	private {
+		File m_file;
+		Entry m_last;
+		bool m_empty;
+		char[] m_buffer;
+	}
+
+	this(File f) {
+		m_buffer.reserve(4096);
+		m_file = f;
+		this.popFront;
+	}
+
+	@property Entry front() { return m_last; }
+	@property bool empty() { return m_empty; }
+
+	void popFront() {
+		import std.string : indexOf, stripLeft;
+		import std.conv : to;
+		immutable read = m_file.readln(m_buffer);
+		if(read == 0) {
+			m_empty = true;
+			return;
+		} else {
+			immutable bar = m_buffer[0..read].indexOf('|');
+
+			if(bar == -1) {
+				m_empty = true;
+				return;
+			} else {
+				auto num = m_buffer[0..bar].stripLeft;
+				if(num.length) {
+					m_last.Used = true;
+					m_last.Count = num.to!size_t;
+				} else {
+					m_last.Used = false;
+					m_last.Count = 0;
+				}
+
+				m_last.Source = m_buffer[bar + 1 .. read].dup;
+			}
+		}
+	}
+}
+
+version(unittest) import fluent.asserts;
+
+@("ByElementRange produces expected results")
+unittest {
+	import std.array : array;
+
+	ByEntryRange(File("sample/hello.lst" ,"r")).array
+		.should.be.equal([
+			Entry(false, 0, "import std.stdio;\n"),
+			Entry(false, 0, "\n"),
+			Entry(false, 0, "void main() {\n"),
+			Entry(true, 1, "        writeln(\"Hello world!\");\n"),
+			Entry(false, 0, "}\n"),
+		]);
+}
