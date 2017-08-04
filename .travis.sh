5eace43fc8fc2464295b986114eedc67e92eba51
Anton Fediushin <fediushin.anton@yandex.ru>
2017-08-04 11:09:36 +0200

    Let ldc2 build release builds

--- .travis.sh
+++ .travis.sh
@@ -6,6 +6,10 @@ if [ "$DC" != gdc ]; then
 	dub test --compiler=$DC
 fi
 
-echo "Building $(git tag -l --points-at HEAD) for $TRAVIS_OS_NAME x86_64 using $DC"
-dub build -b release --compiler=$DC
-tar -zcf "covered-$(git tag -l --points-at HEAD)-$TRAVIS_OS_NAME-$DC-x86_64.tar.gz" covered
+if [ "$DC" == ldc2 ]; then
+	echo "Building $(git tag -l --points-at HEAD) for $TRAVIS_OS_NAME x86_64 using $DC"
+	dub build -b release --compiler=$DC
+	tar -zcf "covered-$(git tag -l --points-at HEAD)-$TRAVIS_OS_NAME-$DC-x86_64.tar.gz" covered
+else
+	dub build -b debug --compiler=$DC
+fi
